9d410de04300e336e3c20e27b97cfa43e2acd3c7
SugiraVinc <sugiravincentg11@gmail.com>
2024-06-26 00:43:21 +0300

    Javascript basics

---
+++ TypeConversion/index.js
@@ -0,0 +1,16 @@
+// console statement
+console.log('Hello World');
+const price = 400;
+console.log(price);  
+
+// object and its properties
+let person= {
+    name: 'Josh',
+    lastName: 'Ishami',
+    age: 30
+};
+// to change the keys of an object
+// Dot Notation
+person.name = 'Shima';
+console.log(person);
+
---
+++ objects and Properties/index.js
@@ -0,0 +1,24 @@
+let amount = '100'
+
+// change type from string to integer/Number
+
+// 1st way by parseInt() method
+amount = parseInt(amount)
+amount = parseFloat(amount) //float
+// secondway by + sign
+
+amount = +amount
+
+// Using Number type
+
+amount = Number(amount)
+console.log(amount, typeof amount)
+
+/* string to a number */
+
+amount = amount.toString();
+amount = string(amount)
+
+console.log(amount, typeof amount)
+
+
